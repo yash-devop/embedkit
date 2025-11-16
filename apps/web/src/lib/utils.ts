9627a19cfe941a4adfe78d8064b23db1e950517b
yash-devop <yashkamble.dev@gmail.com>
2025-11-16 08:07:18 +0530

    util: getInitials for fallback images

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -1,6 +1,27 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const getInitials = (name: string, limit: number = 2) => {
+  if (!name) {
+    return "NA";
+  }
+  let result = "";
+
+  if (name.split(" ")[0] === name) {
+    return name.charAt(0).toUpperCase() + name.slice(1).charAt(0).toUpperCase();
+  }
+  name.split(" ").forEach((item) => {
+    console.log("result in", result.length, limit);
+
+    if (result.length > limit - 1) return;
+    result += item.charAt(0).toUpperCase();
+  });
+
+  return result;
+};
+
+console.log(getInitials("", 3));
