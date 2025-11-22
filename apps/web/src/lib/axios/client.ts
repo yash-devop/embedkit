eb209fd6ce4982d5a0235b57546aee7ee913933e
yash-devop <yashkamble.dev@gmail.com>
2025-11-22 14:29:34 +0530

    feat: axios client

---
+++ apps/web/src/lib/axios/client.ts
@@ -0,0 +1,7 @@
+import { BACKEND_URL } from "@repo/common/config";
+import axios from "axios";
+export const apiClient = axios.create({
+  baseURL: `${BACKEND_URL}/api`,
+  withCredentials: true,
+  timeout: 15000,
+});
