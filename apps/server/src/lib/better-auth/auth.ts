6aa38b18dacfa6040f78690c8fc2a364e5ffe4b3
yash-devop <yashkamble.dev@gmail.com>
2025-11-22 14:32:34 +0530

    feat: add user integration relationship connection as soon as user signin

--- apps/server/src/lib/better-auth/auth.ts
+++ apps/server/src/lib/better-auth/auth.ts
@@ -1,19 +1,33 @@
+import { FRONTEND_URL } from "@repo/common/config";
 import "@repo/common/env";
+import { env } from "@repo/common/env";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { PrismaClient } from "../../../prisma/src/generatedClient/prisma/client.js";
-import { FRONTEND_URL } from "@repo/common/config";
+import { prisma } from "../prisma-orm/prisma.js";
 
-const prisma = new PrismaClient();
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
   socialProviders: {
     github: {
-      clientId: process.env.GITHUB_CLIENT_ID as string,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
     },
   },
   trustedOrigins: [FRONTEND_URL],
+  databaseHooks: {
+    user: {
+      create: {
+        after: async (userData) => {
+          await prisma.githubIntegration.create({
+            data: {
+              userId: userData.id,
+              status: "PENDING",
+            },
+          });
+        },
+      },
+    },
+  },
 });
